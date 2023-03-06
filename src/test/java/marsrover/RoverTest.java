80c93f0bd8b2a4273eff39353d7e38ae72d447b2
AlexisBlanchard <alexis.blanchard@lacombedulionvert.fr>
2023-03-06 14:59:18 +0100

    Split responsability of some RoverTest

--- src/test/java/marsrover/RoverTest.java
+++ src/test/java/marsrover/RoverTest.java
@@ -1,5 +1,6 @@
 package marsrover;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -9,12 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoverTest {
 
+    @Test
+    void shouldInitializeARoverWithItsPosition() {
+        Position position = somePosition();
+        Rover rover = new Rover(mars(), position, Direction.SOUTH);
+        assertThat(rover.getPosition()).isEqualTo(position);
+    }
+
+    @Test
+    void shouldInitializeARoverOnAPlanet() {
+        Planet mars = mars();
+        Rover rover = new Rover(mars, somePosition(), Direction.SOUTH);
+        assertThat(rover.getPosition()).isEqualTo(somePosition());
+    }
+
     @ParameterizedTest
     @EnumSource(Direction.class)
-    void shouldInitializeARoverWithItsPositionAndDirection(Direction direction) {
-        Position position = somePosition();
-        Rover rover = aRover(mars(), position, direction);
-        assertThat(rover.getPosition()).isEqualTo(position);
+    void shouldInitializeARoverWithItsDirection(Direction direction) {
+        Rover rover = new Rover(mars(), somePosition(), direction);
         assertThat(rover.getDirection()).isEqualTo(direction);
     }
 
